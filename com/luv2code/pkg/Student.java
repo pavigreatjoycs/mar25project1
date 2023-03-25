764730d477f14721e92a23cc4a81c0825bcfa72d
Great Joycs <76424765+pavigreatjoycs@users.noreply.github.com>
2023-03-25 10:59:09 +0530

    Update Student.java

--- com/luv2code/pkg/Student.java
+++ com/luv2code/pkg/Student.java
@@ -2,4 +2,11 @@ public class Student{
 	int studentId;
 	String name;
 	
+	public Student(){}
+	
+	pubic Student(int studentId, String name){
+		this.studentId=studentId;
+		this.name=name;
+	}
+	
 }
